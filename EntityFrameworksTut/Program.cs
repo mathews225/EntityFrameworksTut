d30cfd5e7e2cbba5fac7f979587dd0be1b3464fc
mathews225 <petemathews14@gmail.com>
2021-02-26 09:54:21 -0500

    Notes on connection

--- EntityFrameworksTut/Program.cs
+++ EntityFrameworksTut/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Linq;
+
+/* tools > Nuget Package Manager Console
+ * PM> install-package Microsoft.EntityFrameworksCore.Tools
+ * PM> install-package Microsoft.EntityFrameworksCore.SQLServer
+ * PM> scaffold-dbcontext "server=localhost\sqlexpress;database=eddb;trusted_connection=true;" Microsoft.EntityFrameworkCOre.SqlServer -OutputDir Models
+ * 
+ */
 
 namespace EntityFrameworksTut {
 	class Program {
 		static void Main(string[] args) {
-			// tools > Nuget Package Manager Console
-			// PM> install-package Microsoft.EntityFrameworksCore.Tools
-			// PM> install-package Microsoft.EntityFrameworksCore.SQLServer
-			// PM> scaffold-dbcontext "server=localhost\sqlexpress;database=eddb;trusted_connection=true;" Microsoft.EntityFrameworkCOre.SqlServer -OutputDir Models
-			// PM> 
 
 
 
-		}
-	}
+}
+}
 }
