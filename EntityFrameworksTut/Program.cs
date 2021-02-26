a939b37ca32e4c5e4da5d25d9319ff7d85241369
mathews225 <petemathews14@gmail.com>
2021-02-26 10:53:51 -0500

    LINQ

--- EntityFrameworksTut/Program.cs
+++ EntityFrameworksTut/Program.cs
@@ -38,9 +38,43 @@ namespace EntityFrameworksTut {
 			#region Example 3
 
 			_context.Students.ToList().ForEach(s => Console.WriteLine($"{s.Firstname} {s.Lastname}"));
-			
+
 			#endregion
 
+			#region LINQ
+
+			var major = from m in _context.Majors
+									where m.MinSat < 1000
+									orderby m.Description
+									select m;
+
+			Console.WriteLine($"\nMinSat\t Description");
+			foreach (var m in major) {
+				Console.WriteLine($"{m.MinSat} \t {m.Description}");
+			}
+
+			// Join student & Major print name and major
+
+			Console.WriteLine($"\nName \t Description");
+
+			var allStudents = from s in _context.Students
+												join m in _context.Majors
+												on s.MajorId equals m.Id
+												select new {
+													name = s.Lastname + ", " + s.Firstname,
+													major = s.MajorId == null ? "Undeclared" : m.Description
+												};
+
+			foreach (var s in allStudents) {
+				Console.WriteLine($"{s.name} | {s.major}");
+			}
+
+			// allStudents.foreach(s => Console.WriteLine("{0,20}{1,10}",$"{studentsmajor.);
+
+			// 
+
+
+			#endregion
 
 			#endregion
 
